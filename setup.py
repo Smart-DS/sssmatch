25df05dc674adf48ce97c2cdeb636fb99538a621
Elaine Hale <elaine.hale@nrel.gov>
2017-08-28 16:29:43 -0600

    Trying to get package data to install.

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ setup(
     author_email='elaine.hale@nrel.gov',
     packages=['genmatch', 'sssparser'],
     scripts=['bin/gm.py'],
-    package_data={'': ['*.csv','*.gms']},
+    package_data={'genmixes': ['genmixes/**/*.csv']
+                  'genmatch.models': ['genmatch/models/*.csv',
+                                      'genmatch/models/*.gms']},
     url='https://github.com/Smart-DS/genmatch',
     description='Apply NREL Standard Scenario generation mixes to arbitrary transmission systems.',
     install_requires=open('requirements.txt').read(),
