e57056af3431ca70cdf1c89bd95a314361946aac
Elaine Hale <elaine.hale@nrel.gov>
2017-08-28 21:42:07 -0600

    Defining __all__ instead of importing.

--- genmatch/__init__.py
+++ genmatch/__init__.py
@@ -1,6 +1,8 @@
 
 import os
 
+__all__ = ['cli','request','sssdataset']
+
 cli_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),'bin','gm.py')
 datasets_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)),'genmixes')
 models_dir = os.path.join(os.path.dirname(__file__),'models')
