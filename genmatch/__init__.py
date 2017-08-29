d3d2e14d0875a3942c984515af6e95c2cf1259d8
Elaine Hale <elaine.hale@nrel.gov>
2017-08-28 21:31:38 -0600

    Importing modules into packages.

--- genmatch/__init__.py
+++ genmatch/__init__.py
@@ -1,6 +1,10 @@
 
 import os
 
+import .cli
+import .request
+import .sssdataset
+
 cli_path = os.path.join(os.path.dirname(os.path.dirname(__file__)),'bin','gm.py')
 datasets_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)),'genmixes')
 models_dir = os.path.join(os.path.dirname(__file__),'models')
