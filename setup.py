7ff743aed9aec2c0422003adb1958d686536a138
Elaine Hale <elaine.hale@nrel.gov>
2017-08-31 13:03:44 -0600

    data_files are a pain

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from distutils.core import setup
 import glob
 
+data_install_dir = 'Lib/site-packages/'
+dataset_dirs = glob.glob('genmixes/**')
+data_files = []
+for dataset_dir in dataset_dirs:
+    data_files.append((data_install_dir + dataset_dir,glob.glob(dataset_dir + '/*.csv')))
+    for subdir in glob.glob(dataset_dir + '/**'):
+        data_files.append((data_install_dir + subdir,glob.glob(subdir + '/*.csv')))
+
 setup(
     name='genmatch',
     version='0.1.0',
@@ -9,7 +17,7 @@ setup(
     packages=['genmatch', 'sssparser'],
     package_data={'genmatch': ['models/*.csv',
                                'models/*.gms']},
-    data_files=[('genmixes',glob.glob('genmixes/**/*.csv') + glob.glob('genmixes/**/**/*.csv'))],
+    data_files=data_files,
     scripts=['bin/gm.py'],
     url='https://github.com/Smart-DS/genmatch',
     description='Apply NREL Standard Scenario generation mixes to arbitrary transmission systems.',
