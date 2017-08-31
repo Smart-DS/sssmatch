bd2cf8dddcffacc47cfa8344839782fada148cfc
Elaine Hale <elaine.hale@nrel.gov>
2017-08-31 13:53:55 -0600

    One last time?

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ import os
 
 data_files = []
 data_install_dir = os.path.join('Lib','site-packages')
+curdir = None
 for dirpath, dirnames, filenames in os.walk('genmixes'):
     for filename in filenames:
-        data_files.append((os.path.join(data_install_dir,dirpath),os.path.join(dirpath,filename)))
+        if curdir is None or dirpath != curdir:
+            data_files.append((os.path.join(data_install_dir,dirpath),[]))
+            curdir = dirpath
+        data_files[-1][1].append(os.path.join(dirpath,filename))
 
 setup(
     name='genmatch',
@@ -19,6 +23,5 @@ setup(
     data_files=data_files,
     scripts=['bin/gm.py'],
     url='https://github.com/Smart-DS/genmatch',
-    description='Apply NREL Standard Scenario generation mixes to arbitrary transmission systems.',
-    install_requires=open('requirements.txt').read(),
+    description='Apply NREL Standard Scenario generation mixes to arbitrary transmission systems.'
 )
